f8eaf689349c86dd143fdc8f374b473b3fb13551
Vaibhav Lakshmi <114008903+VaibhavLakshmiS@users.noreply.github.com>
2026-03-18 14:11:58 -0400

    Time: 0 ms (100%), Space: 12.4 MB (46.85%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        length = len(nums)
+        if length <=2:
+            return nums.reverse()
+        ptr = length-2
+        while nums[ptr]>=0 and nums[ptr]>=nums[ptr+1]:
+            ptr=ptr-1
+        if ptr == -1:
+            return nums.reverse()
+        for i in range(len(nums)-1, ptr,-1):
+            if nums[ptr]< nums[i]:
+                nums[ptr],nums[i]=nums[i],nums[ptr]
+                break
+        nums[ptr+1:]=reversed(nums[ptr+1:])
